55804f1e099c6618732d90f7c661f585a84bc60d
Marzi <mohammadimarziyeh6367@gmail.com>
2026-07-24 08:33:04 +0330

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,7 +1,114 @@
-alert("جاوااسکریپت وصل شد");
+
+let studentName = "";
+let score = 0;
+
+
+const words = [
+
+{word:"دَر", emoji:"🚪", correct:true},
+{word:"دریا", emoji:"🌊", correct:true},
+{word:"دوست", emoji:"🤝", correct:true},
+{word:"دندان", emoji:"🦷", correct:true},
+{word:"دست", emoji:"✋", correct:true},
+{word:"دارکوب", emoji:"🐦", correct:true},
+{word:"درخت", emoji:"🌳", correct:true},
+
+{word:"باد", emoji:"🌬️", correct:false},
+{word:"سیب", emoji:"🍎", correct:false},
+{word:"گل", emoji:"🌸", correct:false}
+
+];
+
 
 document.getElementById("startBtn").onclick = function(){
 
-alert("دکمه شروع کار کرد");
+studentName =
+document.getElementById("studentName").value;
+
+
+if(studentName==""){
+
+alert("نامت را وارد کن");
+
+return;
+
+}
+
+
+document.getElementById("startPage").style.display="none";
+
+document.getElementById("gamePage").style.display="block";
+
+
+createCards();
 
 };
+
+
+
+function createCards(){
+
+
+let box=document.getElementById("cards");
+
+box.innerHTML="";
+
+
+words.sort(()=>Math.random()-0.5);
+
+
+words.forEach(item=>{
+
+
+let card=document.createElement("button");
+
+
+card.className="wordCard";
+
+
+card.innerHTML=
+item.emoji+"<br>"+item.word;
+
+
+
+card.onclick=function(){
+
+
+if(item.correct){
+
+
+score++;
+
+card.classList.add("correct");
+
+document.getElementById("message").innerHTML=
+"🌟 آفرین درست بود";
+
+
+}
+
+else{
+
+
+card.classList.add("wrong");
+
+document.getElementById("message").innerHTML=
+"😊 دوباره تلاش کن";
+
+
+}
+
+
+document.getElementById("score").innerHTML=score;
+
+
+};
+
+
+box.appendChild(card);
+
+
+});
+
+
+}
